27f0fc434f6cbea768a44aafe5c6e044bf454cbf
Cristian Bastidas <gabrielbastidas5g@outlook.com>
2023-11-29 00:44:15 -0500

    All tested

--- 2015/04_the_ideal_stocking_stuffer/main.py
+++ 2015/04_the_ideal_stocking_stuffer/main.py
@@ -1,12 +1,18 @@
 import hashlib
 
-to_hash = 'ckczppom'
-z = 6  # 5
 
-for i in range(0, 10000000):
-    to_hash_tmp = to_hash + str(i)
-    hash = hashlib.md5(to_hash_tmp.encode('utf-8')).hexdigest()
+def to_hash(s, z):
+    for i in range(0, 10000000):
+        to_hash_tmp = s + str(i)
+        hash = hashlib.md5(to_hash_tmp.encode("utf-8")).hexdigest()
 
-    if hash.startswith('0' * z):
-        print(i)
-        break
+        if hash.startswith("0" * z):
+            return i
+
+
+S = "ckczppom"
+ans1 = to_hash(S, 5)
+ans2 = to_hash(S, 6)
+ans = (ans1, ans2)
+
+print(ans)
