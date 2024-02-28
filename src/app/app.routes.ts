8954fd15674103cfb1f156b17655336b60eeaf8d
Cristian Bastidas <6288158+crixodia@users.noreply.github.com>
2024-02-28 01:19:44 +0000

    :sparkles: Routes based on year event and advent day

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { SolverComponent } from './components/solver/solver.component';
+import { EventComponent } from './components/event/event.component';
 
 export const routes: Routes = [{
     path: '',
     component: HomeComponent
+}, {
+    path: ':year',
+    component: EventComponent
+}, {
+    path: ':year/:day',
+    component: SolverComponent
 }, {
     path: '**',
     redirectTo: ''
