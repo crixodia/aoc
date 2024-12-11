8e48f19b8a851a8c84ef9b30375eb7c23edc29ca
Cristian Bastidas <crixodia@outlook.com>
2024-12-11 11:15:38 -0500

    Day 11: Plutonian Pebbles

--- 2024/11_plutonian_pebbles/main.py
+++ 2024/11_plutonian_pebbles/main.py
@@ -1,57 +1,48 @@
+from time import time
+
+
 def read_input(input_file="input.txt"):
     return open(input_file, "r").readline().replace("\n", "").split()
 
 
-def blink(current):
-    if current == "0":
-        return 0, ["1"]
-    elif len(current) % 2 == 0:
-        mid = len(current) // 2
+def blink(stone):
+    if stone == "0":
+        return ["1"]
 
-        left = str(int(current[:mid]))
-        right = str(int(current[mid:]))
+    elif len(stone) % 2 == 0:
+        mid = len(stone) // 2
 
-        return 1, [left, right]
+        left = str(int(stone[:mid]))
+        right = str(int(stone[mid:]))
 
-    return 0, [str(int(current) * 2024)]
+        return [left, right]
+
+    return [str(int(stone) * 2024)]
 
 
-def solve(puzzle, iters=6):
+# Memory stores the number of stones with the stone label and its depth
+def conunt_stones(stones, depth, memory={(1, "0"): 1}):
+    if depth == 0:
+        return len(stones)
 
-    stones = len(puzzle)
-    last_stones = len(puzzle)
+    n_stones = 0
+    for s in stones:
+        if (depth, s) in memory:
+            n_stones += memory[(depth, s)]
+            continue
 
-    with open("output.txt", "w") as f:
-        for value in puzzle:
-            f.write(value + "\n")
+        new_stones = blink(s)
 
-    current_line = 0
-    level = 0
+        memory[(depth, s)] = conunt_stones(
+            new_stones,
+            depth - 1,
+            memory
+        )
+        n_stones += memory[(depth, s)]
 
-    with open("output.txt", 'r') as f:
-        for line in f:
-
-            size, new_val = blink(line.replace("\n", ""))
-
-            stones += size
-
-            with open("output.txt", "a") as g:
-                for val in new_val:
-                    g.write(f"{val}\n")
-
-            current_line += 1
-            if current_line == last_stones:
-                level += 1
-                last_stones = stones
-                current_line = 0
-                print(level)
-
-            if level >= iters:
-                break
-
-    return stones
+    return n_stones
 
 
 puzzle = read_input()
-ans1 = solve(puzzle, 75)
-print(ans1)
+ans = (conunt_stones(puzzle, 25), conunt_stones(puzzle, 75))
+print(ans)
