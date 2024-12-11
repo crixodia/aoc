0fc82402f789501352a6d2f2946149d7e9155661
Cristian Bastidas <crixodia@outlook.com>
2024-12-11 12:48:02 -0500

    Day 11: Plutonian Pebbles

--- 2024/11_plutonian_pebbles/main.py
+++ 2024/11_plutonian_pebbles/main.py
@@ -1,13 +1,13 @@
-from time import time
+from functools import lru_cache
 
 
 def read_input(input_file="input.txt"):
-    return open(input_file, "r").readline().replace("\n", "").split()
+    return tuple(open(input_file, "r").readline().replace("\n", "").split())
 
 
 def blink(stone):
     if stone == "0":
-        return ["1"]
+        return tuple(["1"])
 
     elif len(stone) % 2 == 0:
         mid = len(stone) // 2
@@ -15,34 +15,25 @@ def blink(stone):
         left = str(int(stone[:mid]))
         right = str(int(stone[mid:]))
 
-        return [left, right]
+        return (left, right)
 
-    return [str(int(stone) * 2024)]
+    return tuple([str(int(stone) * 2024)])
 
 
-# Memory stores the number of stones with the stone label and its depth
-def conunt_stones(stones, depth, memory={(1, "0"): 1}):
+# Use LRU cache to optimize stone counting
+@lru_cache(None)
+def count_stones(stones, depth):
     if depth == 0:
-        return len(stones)
+        return len(stones)  # A single stone remains
 
     n_stones = 0
     for s in stones:
-        if (depth, s) in memory:
-            n_stones += memory[(depth, s)]
-            continue
-
         new_stones = blink(s)
-
-        memory[(depth, s)] = conunt_stones(
-            new_stones,
-            depth - 1,
-            memory
-        )
-        n_stones += memory[(depth, s)]
+        n_stones += count_stones(new_stones, depth - 1)
 
     return n_stones
 
 
 puzzle = read_input()
-ans = (conunt_stones(puzzle, 25), conunt_stones(puzzle, 75))
+ans = (count_stones(puzzle, 25), count_stones(puzzle, 75))
 print(ans)
