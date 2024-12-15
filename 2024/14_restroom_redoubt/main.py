cf7635f88716f0d85ee937cc358bb256cc8a2201
Cristian Bastidas <crixodia@outlook.com>
2024-12-15 13:34:00 -0500

    Day 14: Restroom Redoubt

--- 2024/14_restroom_redoubt/main.py
+++ 2024/14_restroom_redoubt/main.py
@@ -1,3 +1,31 @@
+from math import inf
+import os
+
+from PIL import Image
+
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def draw_binary_matrix(matrix, output_path):
+    colors = {
+        0: (128, 128, 128),  # Grey
+        1: (255, 165, 0)    # Orange
+    }
+
+    rows = len(matrix)
+    cols = len(matrix[0]) if rows > 0 else 0
+
+    img = Image.new('RGB', (cols, rows))
+    pixels = img.load()
+
+    for y in range(rows):
+        for x in range(cols):
+            pixels[x, y] = colors[matrix[y][x]]
+
+    img = img.resize((1000, 1000), resample=Image.NEAREST)
+    img.save(output_path)
 
 
 def read_input(input_file="input.txt"):
@@ -21,11 +49,13 @@ def read_input(input_file="input.txt"):
 def solve(puzzle, rows=7, cols=11, seconds=100):
     P, V = puzzle
     mid_col, mid_row = cols//2, rows//2
+    current_min = inf
 
-    for t in range(0, seconds + 1):
-        bathroom = [[' ']*cols for _ in range(rows)]
+    for t in range(seconds + 1):
+        bathroom = [[0]*cols for _ in range(rows)]
         if t == 0:
             continue
+
         for r, p in enumerate(P):
             x, y = p
             vx, vy = V[r]
@@ -37,22 +67,32 @@ def solve(puzzle, rows=7, cols=11, seconds=100):
             y = y % rows
 
             P[r] = [x, y]
-            bathroom[y][x] = "#"
+            bathroom[y][x] = 1
 
-    q1, q2, q3, q4 = 0, 0, 0, 0
-    for r, p in enumerate(P):
-        x, y = p
-        if x < mid_col and y < mid_row:
-            q1 += 1
-        elif x > mid_col and y < mid_row:
-            q2 += 1
-        elif x < mid_col and y > mid_row:
-            q3 += 1
-        elif x > mid_col and y > mid_row:
-            q4 += 1
+        q1, q2, q3, q4 = 0, 0, 0, 0
+        for r, p in enumerate(P):
+            x, y = p
+            if x < mid_col and y < mid_row:
+                q1 += 1
+            elif x > mid_col and y < mid_row:
+                q2 += 1
+            elif x < mid_col and y > mid_row:
+                q3 += 1
+            elif x > mid_col and y > mid_row:
+                q4 += 1
 
-    return q1 * q2 * q3 * q4, bathroom
+        sf = q1*q2*q3*q4
+
+        if sf < current_min:
+            clear_console()
+            draw_binary_matrix(bathroom, f"./output/{t}.png")
+            current_min = sf
+            print(t, current_min)
+
+    return sf
 
 
-ans = (solve(read_input(), rows=103, cols=101), solve2(read_input()))
-print(ans)
+ans = (solve(read_input(), rows=103, cols=101))
+print("Part 1:", ans)
+input("Press enter to continue...")
+solve(read_input(), 103, 101, 100_000_000_000)
